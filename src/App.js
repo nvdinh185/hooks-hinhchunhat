f044c2e8918424bbe78b9415892de8a3ace31077
nvdinh185 <nvdinh0766777123@gmail.com>
2023-09-27 17:54:07 +0700

    advance

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 const App = () => {
     const [chuVi, setChuVi] = useState('');
     const [dienTich, setDienTich] = useState('');
+    const [errorCD, setErrorCD] = useState('');
+    const [errorCR, setErrorCR] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,6 +20,35 @@ const App = () => {
         if (chieuDai && chieuRong) {
             setChuVi((chieuDai + chieuRong) * 2);
             setDienTich(chieuDai * chieuRong);
+        } else {
+            if (!chieuDai) {
+                setErrorCD('Vui lòng nhập chiều dài');
+            }
+            if (!chieuRong) {
+                setErrorCR('Vui lòng nhập chiều rộng');
+            }
+            setChuVi('')
+            setDienTich('')
+        }
+    }
+
+    const handleBlur = (e) => {
+        if (e.target.name == 'chieudai') {
+            if (!e.target.value) {
+                setErrorCD('Vui lòng nhập chiều dài');
+            }
+        } else if (e.target.name == 'chieurong') {
+            if (!e.target.value) {
+                setErrorCR('Vui lòng nhập chiều rộng');
+            }
+        }
+    }
+
+    const handleInput = (e) => {
+        if (e.target.name == 'chieudai') {
+            setErrorCD('')
+        } else if (e.target.name == 'chieurong') {
+            setErrorCR('');
         }
     }
 
@@ -26,12 +57,20 @@ const App = () => {
             <form onSubmit={(e) => handleSubmit(e)}>
                 <div>
                     <label>Chiều dài</label>
-                    <input type="text" name="chieudai" />
+                    <input onBlur={(e) => handleBlur(e)} onInput={(e) => handleInput(e)} type="text" name="chieudai" />
+                    <span style={{
+                        color: 'red',
+                        fontStyle: 'italic'
+                    }}>{errorCD}</span>
                 </div>
                 <br />
                 <div>
                     <label>Chiều rộng</label>
-                    <input type="text" name="chieurong" />
+                    <input onBlur={(e) => handleBlur(e)} onInput={(e) => handleInput(e)} type="text" name="chieurong" />
+                    <span style={{
+                        color: 'red',
+                        fontStyle: 'italic'
+                    }}>{errorCR}</span>
                 </div>
                 <input type="submit" value="Tinh" />
             </form>
